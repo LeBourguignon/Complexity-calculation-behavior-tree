d795712d1bb78ba47f25583102b5bb029a2e3d46
Baptiste Andres <63340452+LeBourguignon@users.noreply.github.com>
2023-12-04 21:43:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,16 @@ from koopa_paratroopa_vert import koopa_paratroopa_vert_state_graph
 if __name__ == "__main__":
     main_graph = goomba_state_graph()
     print("Goomba :")
-    # calculate_complexity_score(main_graph, True)
     analyze_state_graph(main_graph, True)
 
     main_graph = piranha_plant_state_graph()
     print("Piranha plant :")
-    # calculate_complexity_score(main_graph, True)
     analyze_state_graph(main_graph, True)
 
     main_graph = koopa_troopa_vert_state_graph()
     print("Koopa troopa vert :")
-    # calculate_complexity_score(main_graph, True)
     analyze_state_graph(main_graph, True)
 
     main_graph = koopa_paratroopa_vert_state_graph()
     print("Koopa paratroopa vert :")
-    # calculate_complexity_score(main_graph, True)
     analyze_state_graph(main_graph, True)
